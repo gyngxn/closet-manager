cd8e61f056ab6cf559f1a9bf2eeaf02b54283d6e
gyngxn_ <jdj0311028@naver.com>
2025-11-26 23:21:19 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/closet_manager/ClosetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.closet_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetManagerApplication.class, args);
+	}
+
+}
